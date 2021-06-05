287a1032d3d0a9847caff30846ce9f68a71992da
ntrasher <nickrak10@gmail.com>
2021-06-05 11:43:59 -0300

    Inicializacion del proyecto Spring

---
+++ Backend/reproweb/src/main/java/com/reproweb/reproweb/ReprowebApplication.java
@@ -0,0 +1,13 @@
+package com.reproweb.reproweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReprowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReprowebApplication.class, args);
+	}
+
+}
---
+++ Backend/reproweb/src/test/java/com/reproweb/reproweb/ReprowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reproweb.reproweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReprowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
