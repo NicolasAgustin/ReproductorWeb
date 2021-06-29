f6f2670a78481f3cf4afd094c62e1c02ea0f844c
matias <matighersevich@gmail.com>
2021-06-28 21:21:08 -0300

    Paginas HTML

--- Frontend/Reproductor/src/app/app-routing.module.ts
+++ Frontend/Reproductor/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"tutorial" , component: TutorialComponent }, {path:"principal" , component : PrincipalComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
