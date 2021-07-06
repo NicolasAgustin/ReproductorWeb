d89a3b8628034da7fdeb3f87cca025706cc43b98
NicolasAgustin <nickrak10@gmail.com>
2021-07-06 20:13:47 -0300

    Agregado componente tutorial

--- Frontend/Reproductor/src/app/tutorial/tutorial.component.ts
+++ Frontend/Reproductor/src/app/tutorial/tutorial.component.ts
@@ -8,9 +8,25 @@ import { } from '@angular/core';
 })
 export class TutorialComponent implements OnInit {
 
-  constructor() { }
+  public playing: boolean;
+  public titulo: string;
+
+  constructor() {
+    this.playing = false;
+    this.titulo = 'Reanuda la canción en reproducción';
+  }
 
   ngOnInit(): void {
+  }
+
+  changeState(){
+    this.playing = !this.playing;
+    if(this.playing) this.titulo = "Pausa la canción en reproducción"; else this.titulo = "Reanuda la canción en reproducción";
+    // if (this.playing){
+    //   this.playing = false;
+    // }  else {
+    //   this.playing = true;
+    // }
   }
 
 }
